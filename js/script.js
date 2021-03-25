fa8d8f828fc1cead12b983a014841e0784d0124a
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-25 15:57:01 +0100

    add. click

--- js/script.js
+++ js/script.js
@@ -4,7 +4,44 @@ var app = new Vue({
 
   data:{
 
-    
+    imgSlider: [
+
+      "img/photo1.jpg",
+
+      "img/photo2.jpg",
+
+      "img/photo3.jpg"
+
+    ],
+
+    indexImg:0,
+
+  },
+
+  methods: {
+
+    rightBtn: function(){
+
+      this.indexImg++;
+
+      if(this.indexImg == this.imgSlider.length){
+
+        this.indexImg = 0;
+
+      }
+    },
+
+    leftBtn: function(){
+
+      this.indexImg--;
+
+      if(this.indexImg == -1){
+
+        this.indexImg = this.imgSlider.length-1;
+
+      }
+    }
+
 
   }
 
